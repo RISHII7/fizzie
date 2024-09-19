660de8fac857079909d76c431a8d208423b704bc
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-19 20:03:11 +0530

    Handling the mobile version

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -19,6 +19,7 @@ import Scene from "./Scene";
 import { Bubbles } from "./Bubbles";
 
 import { useStore } from "@/hooks/useStore";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -33,9 +34,10 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 const Hero = ({ slice }: HeroProps): JSX.Element => {
 
   const ready = useStore((state) => state.ready);
+  const isDesktop = useMediaQuery("(min-width:768px)",true)
 
   useGSAP(() => {
-    if (!ready) return;
+    if (!ready && isDesktop) return;
 
     const introTl = gsap.timeline();
 
@@ -92,7 +94,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           y: 20,
           opacity: 0,
         })
-  }, {dependencies: [ready]});
+  }, {dependencies: [ready, isDesktop]});
 
   return (
     <Bounded
@@ -100,10 +102,12 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className="hero opacity-0"
     >
-      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
-        <Scene />
-        <Bubbles count={300} speed={2} repeat={true} />
-      </View>
+      {isDesktop && (
+        <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+          <Scene />
+          <Bubbles count={300} speed={2} repeat={true} />
+        </View>
+      )}
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
