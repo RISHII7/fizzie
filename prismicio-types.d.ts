89a672be3ec8009a8cc66446cdf0aba14cdc6758
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-20 18:25:58 +0530

    Diving Can Slice Creation and animation using GSAP and THREE.js

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,7 +4,7 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type PageDocumentDataSlicesSlice = HeroSlice;
+type PageDocumentDataSlicesSlice = SkyDIveSlice | HeroSlice;
 
 /**
  * Content for Page documents
@@ -190,6 +190,65 @@ type HeroSliceVariation = HeroSliceDefault;
  */
 export type HeroSlice = prismic.SharedSlice<"hero", HeroSliceVariation>;
 
+/**
+ * Primary content in *SkyDIve → Default → Primary*
+ */
+export interface SkyDIveSliceDefaultPrimary {
+  /**
+   * Sentence field in *SkyDIve → Default → Primary*
+   *
+   * - **Field Type**: Text
+   * - **Placeholder**: *None*
+   * - **API ID Path**: sky_d_ive.default.primary.sentence
+   * - **Documentation**: https://prismic.io/docs/field#key-text
+   */
+  sentence: prismic.KeyTextField;
+
+  /**
+   * Flavor field in *SkyDIve → Default → Primary*
+   *
+   * - **Field Type**: Select
+   * - **Placeholder**: *None*
+   * - **Default Value**: lemonLime
+   * - **API ID Path**: sky_d_ive.default.primary.flavor
+   * - **Documentation**: https://prismic.io/docs/field#select
+   */
+  flavor: prismic.SelectField<
+    "lemonLime" | "grape" | "blackCherry" | "strawberryLemonade" | "watermelon",
+    "filled"
+  >;
+}
+
+/**
+ * Default variation for SkyDIve Slice
+ *
+ * - **API ID**: `default`
+ * - **Description**: Default
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type SkyDIveSliceDefault = prismic.SharedSliceVariation<
+  "default",
+  Simplify<SkyDIveSliceDefaultPrimary>,
+  never
+>;
+
+/**
+ * Slice variation for *SkyDIve*
+ */
+type SkyDIveSliceVariation = SkyDIveSliceDefault;
+
+/**
+ * SkyDIve Shared Slice
+ *
+ * - **API ID**: `sky_d_ive`
+ * - **Description**: SkyDIve
+ * - **Documentation**: https://prismic.io/docs/slice
+ */
+export type SkyDIveSlice = prismic.SharedSlice<
+  "sky_d_ive",
+  SkyDIveSliceVariation
+>;
+
 declare module "@prismicio/client" {
   interface CreateClient {
     (
@@ -208,6 +267,10 @@ declare module "@prismicio/client" {
       HeroSliceDefaultPrimary,
       HeroSliceVariation,
       HeroSliceDefault,
+      SkyDIveSlice,
+      SkyDIveSliceDefaultPrimary,
+      SkyDIveSliceVariation,
+      SkyDIveSliceDefault,
     };
   }
 }
